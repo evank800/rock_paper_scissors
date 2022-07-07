d6d881cca7ef38dde279b150587ad381bab6323f
evan <evank800@gmail.com>
2022-07-07 14:42:07 +0200

    Add function playRound

--- game.js
+++ game.js
@@ -11,3 +11,54 @@ function computerPlay(){
 }
 
 console.log(computerPlay())
+
+function playRound(playerSelection, computerSelection){
+    let player = playerSelection.toLowerCase()
+    let computer = computerSelection.toLowerCase()
+
+    if (player == 'rock'){
+        if (computer == 'scissors'){
+            let str = `You Win! ${player} beats ${computer}`
+            return str
+        }
+        else if(computer == 'paper'){
+            let str = `You Lose! ${computer} beats ${player}`
+            return str
+        }
+        else{
+            let str = 'No Winner! Play again'
+            return str
+        }
+    }
+
+    else if (player == 'scissors'){
+        if (computer = 'paper'){
+            let str = `You Win! ${player} beats ${computer}`
+            return str
+        }
+        else if (computer = 'rock'){
+            let str = `You Lose! ${computer} beats ${player}`
+            return str
+        }
+        else{
+            let str = 'No Winner! Play again'
+            return str
+        }
+    }
+
+    else{
+        if (computer == 'rock'){
+            let str = `You Win! ${player} beats ${computer}`
+            return str
+        }
+        else if (computer == 'scissors'){
+            let str = `You Lose! ${computer} beats ${player}`
+            return str
+        }
+        else{
+            let str = 'No Winner! Play again'
+            return str
+        }
+    }
+}
+console.log(playRound('rock', computerPlay()))
